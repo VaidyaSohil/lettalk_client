8645d98c23f1f64fec233215c2b842cfaf088656
Khang96 <nnbaokhang@gmail.com>
2021-03-27 01:34:15 -0600

    Update CORS.js

--- src/components/LandingPage/CORS.js
+++ src/components/LandingPage/CORS.js
@@ -1,1 +1,22 @@
+import React, { useState } from "react";
 
+
+class Cors extends  React.Component {
+    constructor(props) {
+        super(props);
+        this.onClick = this.onClick.bind(this)
+    }
+
+    onClick(){
+      fetch("https://google.com")
+      .then(res => res.json())
+      
+    }
+    render() {
+        return (
+            <div>
+                <Button variant="success"  onClick={this.onClick}>Check CORS</Button>
+            </div>
+        )
+    }
+}
