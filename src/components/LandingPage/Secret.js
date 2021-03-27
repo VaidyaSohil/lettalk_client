f09a33ea0b699ccbb84a427a2f670b6ee68c1c11
Khang96 <nnbaokhang@gmail.com>
2021-03-27 10:42:53 -0600

    Create Secret.js

---
+++ src/components/LandingPage/Secret.js
@@ -0,0 +1,24 @@
+import React, { useState } from "react";
+import {Button} from "react-bootstrap";
+
+
+class Secret extends  React.Component {
+    constructor(props) {
+        super(props);
+        this.onClick = this.onClick.bind(this)
+    }
+
+    onClick(){
+      fetch("https://pure-beach-98773.herokuapp.com/v1/cars/secret",{mode: 'cors'})
+      .then(res => res.json())
+      
+    }
+    render() {
+        return (
+            <div>
+                <Button variant="success"  onClick={this.onClick}>Secret</Button>
+            </div>
+        )
+    }
+}
+export default Cors;
