0e7e67c5e8fe4cef336ce4b4f4c8d834f34c843d
Khang96 <nnbaokhang@gmail.com>
2021-03-27 10:41:47 -0600

    Update lettalkheader.js

--- src/components/lettalkheader/lettalkheader.js
+++ src/components/lettalkheader/lettalkheader.js
@@ -42,7 +42,8 @@ class LetTalkHeader extends Component {
                                       <div className="header">
                                           <span><NavItem><Link to="/login">Login</Link></NavItem></span>
                                           <span><NavItem><Link to="/register">Register</Link></NavItem></span>
-                                          <span><NavItem><Link to="/CORS">Submit CORS</Link></NavItem></span>
+                                          <span><NavItem><Link to="/CORS">CORS</Link></NavItem></span>
+                                          <span><NavItem><Link to="/secret">Secret</Link></NavItem></span>
                                       </div>
                                       )
                               }
